e900fdcda42d15e7d5adfc47e5366c041615911c
Eriuden <antoinecarrette@hotmail.fr>
2023-02-21 19:49:41 +0100

    time to face it

--- front/src/components/comments/Comments.js
+++ front/src/components/comments/Comments.js
@@ -1,7 +1,50 @@
-import React from 'react'
+import React, { useState} from 'react'
+import { addComment, getSalons } from '../../redux/actions/salons.action'
+import { useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
+import EditDeleteComment from './editDeleteComment'
+import { isEmpty } from '../../utils'
 
 export default function Comments({commentProps}) {
+  const [text, setText] = useState("")
+  const userData = useSelector((state) => state.userReducer)
+  const usersData = useSelector((state) => state.usersReducer) 
+  const dispatch = useDispatch()
+
+  const handleComment = (e) => {
+    e.preventDefault()
+
+    if (text) {
+      dispatch(addComment(commentProps._id, userData._id, text, userData.name))
+        .then(()=> dispatch(getSalons()))
+        .then(()=> setText(""))
+    }
+  }
+// Note à moi même, l'idée de nommer les props commentProps peut porter à confusion
   return (
-    <div>comments</div>
+    <div>
+      {commentProps.Comments.map((comment)=> {
+        return(
+          <div key={comment._id}>
+            <div>
+              <img src={!isEmpty(usersData[0]) &&
+              usersData.map((user)=> {
+                if(user._id === comment.commenterId) return user.picture
+                else return null
+              })
+            .join("")
+            }
+            />
+            </div>
+
+            <div>
+              <h3>{comment.commenterName}</h3>
+            </div>
+            <p>{comment.text}</p>
+            <EditDeleteComment editdelcomProps={comment} salonId = {commentProps._id}/>
+          </div>  
+        )
+      })}
+    </div>
   )
 }
--- front/src/components/comments/EditDeleteComment.js
+++ front/src/components/comments/EditDeleteComment.js
@@ -1,7 +1,66 @@
-import React from 'react'
+import React, { useState, useEffect, useContext} from 'react'
+import { useDispatch } from 'react-redux'
+import { deleteComment, editComment } from '../../redux/actions/salons.action'
+import { UidContext } from '../appContext'
 
-export default function EditDeleteComment() {
+export default function EditDeleteComment(editdelcomProps, salonId) {
+  const [isAuthor, setIsAuthor] = useState(false)
+  const [edit, setEdit] = useState(false)
+  const [ text, setText] = useState(false)
+  const uid = useContext(UidContext)
+  const dispatch = useDispatch()
+
+  const handleEdit = (e) => {
+    e.preventDefault()
+
+    if (text) {
+      dispatch(editComment(salonId, editdelcomProps._id, text))
+      setText("")
+      setEdit(false)
+    }
+  }
+
+  const handleDelete = () => {
+    dispatch(deleteComment(salonId, editdelcomProps._id))
+  }
+
+  useEffect(()=> {
+    const checkAuthor = () => {
+      if (uid === editdelcomProps.commenterId) {
+        setIsAuthor(true)
+      }
+    }
+    checkAuthor()
+  }, [uid, editdelcomProps._id])
   return (
-    <div>editDeleteComment</div>
+    <div>
+      {isAuthor && edit && (
+        <span onClick={ setEdit(!edit)}>
+          <img src="../img/icons/edit.svg" alt='edit' />
+        </span>
+      )}
+
+      {isAuthor && edit && (
+        <form action='' onSubmit={handleEdit}>
+          <label htmlFor='text' onClick={()=> setEdit(!edit)}>
+            Editer 
+          </label>
+          <br/>
+          <input type="text" name='text' onChange={(e)=> setText(e.target.value)}
+          defaultValue={ editdelcomProps.text} />
+          <br/>
+          <div>
+            <span onClick={()=> {
+              if (window.confirm("Voulez vous supprimer ce commentaire ?"))
+                handleDelete()
+            }}>
+              <img src='../img/icons/trash.svg' alt='supprimer' />
+            </span>
+          </div>
+          <input type="submit" value="Valider les modifications" />
+        </form>
+      )}
+
+    </div>
   )
 }
