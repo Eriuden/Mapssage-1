bb47cf76935c4088057096f2d3e2dd4b5b68a316
Eriuden <antoinecarrette@hotmail.fr>
2023-05-08 22:06:43 +0200

    fixing

--- front/src/pages/salonPage.js
+++ front/src/pages/salonPage.js
@@ -12,12 +12,11 @@ plus bas, le type de massage, les types de clients, masseurs-masseuses, etc
 encore plus bas, les avis/commentaires
 */ 
 export default function SalonPage() {
-  const user = useSelector((state)=> state.userReducer)
   const salon = useSelector((state)=> state.salonReducer)
   const dispatch = useDispatch()
 
   useEffect(()=> {
-    getSalons(salon._id)
+    dispatch(getSalons(salon._id))
   })
   return (
     <div>
